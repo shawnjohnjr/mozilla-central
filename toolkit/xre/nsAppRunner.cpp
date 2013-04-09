dafe971dbb7cd247ae6f16d1ba39b91caf9b240c
Shawn Huang <shuang@mozilla.com>
2013-04-09 16:53:16 +0800

    Enable crash reporter

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -2997,6 +2997,7 @@ XREMain::XRE_mainInit(bool* aExitFlag)
     return 1;
 
 #ifdef MOZ_CRASHREPORTER
+  SaveToEnv("MOZ_CRASHREPORTER=1");
   if (EnvHasValue("MOZ_CRASHREPORTER")) {
     mAppData->flags |= NS_XRE_ENABLE_CRASH_REPORTER;
   }
